64cebc17c43d284bdb6848853dbbeed998b899aa
Tom Zanussi <tom.zanussi@linux.intel.com>
2015-04-13 10:23:43 -0500

    initscripts: Remove devtmpfs check for proc/filestems
    proc/filesystems may not exist, so don't bail out if not there (until
    we have proper handling for this).
    
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>

--- recipes-core/initscripts/initscripts-1.0/sysfs_devtmpfs.sh
+++ recipes-core/initscripts/initscripts-1.0/sysfs_devtmpfs.sh
@@ -9,6 +9,4 @@
 # Description:       Mount the devtmpfs file system..
 ### END INIT INFO
 
-if [ -e /dev ] && grep -q devtmpfs /proc/filesystems; then
-	mount -t devtmpfs devtmpfs /dev
-fi
+mount -t devtmpfs devtmpfs /dev
